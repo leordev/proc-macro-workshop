a3964becc7e35e9b6f3e3d16b0817dc6d04f0e49
Leo Ribeiro <leordev@gmail.com>
2020-01-19 21:06:27 -0500

    builder-1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,14 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::{quote};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let output = quote!{ #input };
+    println!("{:?}", output);
+    // TokenStream::from(output)
+    TokenStream::new()
 }
